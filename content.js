7e23916901a789b004242151a5e2bffe75c30b48
Gabor Szekely <szvgabor@gmail.com>
2021-11-10 23:12:29 +0100

    Added coloring for matched elements

--- content.js
+++ content.js
@@ -1,34 +1,47 @@
+const typedClass = 'typing-ext-typed'
 let buffer = '';
+let coloredElements = [];
 
 
 document.addEventListener('keydown', (ev) => {
     updateBuffer(ev);
-    updateDOM();
+    let res = searchDOM(buffer);
+    updateDOM(res);
 });
 
-function updateDOM() {
-    if (buffer.length > 4) {
-        xpathSearch(buffer);
+function updateDOM(elements) {
+    for (let el of coloredElements) {
+        if (!elements.includes(el)) {
+            el.classList.remove(typedClass);
+        }
     }
+    for (let el of elements) {
+        if (!el.classList.contains(typedClass)) {
+            el.classList.add(typedClass);
+        }
+    }
+    coloredElements = elements;
+}
+
+function searchDOM(query) {
+    if (buffer.length > 4) {
+        return xpathStringSearch(query);
+    }
+    return [];
 }
 
 function updateBuffer(ev) {
-    // console.log(ev);
-    if (ev.code === 'Space') {
-        ev.preventDefault();
+    if (keypressPreventDefault(ev)) {
+        return;
     }
+
     switch (ev.code) {
-        case 'Space':
-            ev.preventDefault();
-            break;
         case 'Backspace':
-            ev.preventDefault();
             if (buffer.length > 1) {
                 buffer = buffer.slice(0, buffer.length-1);
             }
             break;
         case 'Delete':
-            ev.preventDefault();
             buffer = '';
             break;
     }
@@ -36,10 +49,18 @@ function updateBuffer(ev) {
     if (ev.key.length == 1) {
         buffer += ev.key;
     }
+
     console.log(buffer);
 }
 
-function xpathSearch(query) {
+let previousXpathStringSearch = [null, null];
+function xpathStringSearch(query) {
+    let [prevQuery, prevResult] = previousXpathStringSearch;
+    if (prevQuery === query) {
+        console.warn("Memoized result used in xpathStringSearch!");
+        return [...prevResult];
+    }
+
     let escapedQuery = query.replace("'", "\\'");
     escapedQuery = `.//*[contains(., '${escapedQuery}')]`;
     let results = xpath(escapedQuery, document);
@@ -50,17 +71,20 @@ function xpathSearch(query) {
     try {
         do {
             thisNode = results.iterateNext();
-            if (thisNode) {
-                if (countXPathResultLength(xpath(escapedQuery, thisNode)) === 0) {
+            if (thisNode &&
+                countXPathResultLength(xpath(escapedQuery, thisNode)) === 0 &&
+                isVisible(thisNode)) {
                     validResults.push(thisNode);
-                }
             }
         } while (thisNode) 
     }
     catch (e) {
       console.trace(e);
     }
+
     console.info(validResults);
+    previousXpathStringSearch = [query, [...validResults]]
+
     return validResults;
 }
 
@@ -72,6 +96,26 @@ function countXPathResultLength(res) {
     return count;
 }
 
+function isVisible(element) {
+    let style = window.getComputedStyle(element);
+    return (style.display !== 'none')
+}
+
 function xpath(query, node) {
     return document.evaluate(query, node, null, XPathResult.ANY_TYPE, null);
+}
+
+const preventedKeys = ['Space', 'Backspace', 'Delete'];
+const ignoredKeys = ['Space', 'Backspace', 'Delete'];
+let preventKeyState = true;
+function keypressPreventDefault(ev) {
+    if (ev.code === 'F10') {
+        ev.preventDefault();
+        preventKeyState = !preventKeyState;
+        return true;
+    }
+    if (preventKeyState && preventedKeys.includes(ev.code)) {
+        ev.preventDefault();
+    }
+    return false;
 }
